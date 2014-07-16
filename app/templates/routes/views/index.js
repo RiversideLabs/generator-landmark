a07aac493833a4567bee0c3998e6d3757ff550f2
Mike Stecker <mike@stecker.me>
2014-07-16 10:17:00 -0700

    Updated code for consistency

--- app/templates/routes/views/index.js
+++ app/templates/routes/views/index.js
@@ -2,8 +2,8 @@ var landmark = require('landmark-serve');
 
 exports = module.exports = function(req, res) {
 	
-	var locals = res.locals,
-		view = new landmark.View(req, res);
+	var view = new landmark.View(req, res),
+		locals = res.locals;
 	
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
