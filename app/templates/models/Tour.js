2554850d058bd6ee6ec62e95261982e7e59c97db
Mike Stecker <mike@stecker.me>
2014-07-25 15:57:33 -0700

    Updating tours model

--- app/templates/models/Tour.js
+++ app/templates/models/Tour.js
@@ -17,7 +17,7 @@ Tour.add({
 	title: { type: String, required: true },
 	publishedDate: { type: Date, noedit: true, collapse: true, default: Date.now },
 	lastModified: { type: Date, noedit: true, collapse: true },
-	location: { type: Types.Relationship, ref: 'Location', hidden: true },
+	location: { type: Types.Relationship, ref: 'Location' },
 	type: { type: Types.Select, options: 'audio, video', default: 'audio', index: true },
 	url: { type: Types.S3File, label: 'Video/Audio File', allowedTypes:['audio/mp4', 'audio/mp3', 'audio/mpeg', 'video/mp4', 'video/x-m4v'], note: '.MP3 (audio) or .MP4/.M4V (video) files only. Amazon S3 must be configured in your app settings.' },
 	length: { type: Types.Text }
@@ -29,17 +29,19 @@ Tour.schema.pre('save', function(next) {
 });
 
 Tour.schema.post('save', function(done) {
-  // var doc = this;
-  // if(doc.location) {
-  //   landmark.list('Location').model.findOne().where('_id',doc.location).exec(function(err,data){
-  //     data.tours.push(doc);
-  //     data.save(function(err) {
-  //       if(err)console.log(err);
-  //       //console.log('end pre save txlog');
-  //       //ret();
-  //     });
-  //   });
-  // }
+  var doc = this;
+  if(doc.location) {
+    landmark.list('Location').model.findOne().where('_id',doc.location).exec(function(err,data){
+			if(data.tours.indexOf(doc._id) === -1) {
+				data.tours.push(doc);
+			}
+      data.save(function(err) {
+        if(err)console.log(err);
+        //console.log('end pre save txlog');
+        //ret();
+      });
+    });
+  }
 });
 
 Tour.relationship({ ref: 'Location', path: 'tours' });
